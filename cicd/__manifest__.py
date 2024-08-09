34e0a54bf4a6a680d3c8db503046401b148b14ce
klausmonga <monga.wakowa@gmail.com>
2024-08-09 06:00:05 +0200

    also init files

---
+++ cicd/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'CI/CD',
+    'summary': '',
+    'category' : '',
+    'version': '17.0.1.0',
+    'license': 'OPL-1',
+    'depends' : ['project',],
+    'author': 'bvortex',
+    'website': 'www.bvortex.com',
+    'description': """
+     
+    """,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/project_project_views.xml",
+        # 'report/action_ticket.xml',
+        # 'report/ticket_external_layout.xml',
+        # 'report/report_ticket.xml',
+        # 'views/appointment_view.xml'
+        # 'views/pricelist.xml',
+    ],
+    'assets': {
+
+    },
+    'images': [
+        'static/description/icon.png',
+    ],
+    'installable': True,
+    'application': False,
+}
